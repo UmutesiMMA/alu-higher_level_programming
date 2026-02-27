64264d6ec01c605eb717337d5b134a158edd64e1
Magnificat Marie Augusta Umutesi <m.umutesi@alustudent.com>
2026-02-27 09:09:26 -0800

    add missing line

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
 from add_0 import add
 a = 1
 b = 2
-print("{a} + {b} = {sum}".format(a=a, b=b, sum=add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
