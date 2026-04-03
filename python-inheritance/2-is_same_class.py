d20402d66b58b98a719d82ee2d837c933d174802
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-04-03 20:52:08 +0200

    add answers for python inheritance questions

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that returns true if the instance is trully specified
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    we have to define is instance for obj and a_class
+    """
+
+    return type(obj) == a_class
