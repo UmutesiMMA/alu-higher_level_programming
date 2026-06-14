14802967fb39d18a84eb92f9fa68ccedd82771ff
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-06-14 14:04:16 +0200

    correct logic for 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 if (argv[2] !== undefined) {
-  const arguments = argv.slice(2);
-  arguments.forEach((element) => {
-    console.log(element);
-  });
+  console.log(argv[2]);
 } else {
   console.log("No argument");
 }
