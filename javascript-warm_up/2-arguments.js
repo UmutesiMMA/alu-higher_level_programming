6f221b4275e3740e0ad9ae3b45a5ddb9c36e1ec7
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-06-14 12:58:43 +0200

    correct logic for question 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 let argv = process.argv;
-if (argv.length == 0) {
+if (argv.slice(2).length == 0) {
   console.log("No argument");
-} else if (argv[2] != undefined) {
+} else if (argv.slice(2).length == 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
