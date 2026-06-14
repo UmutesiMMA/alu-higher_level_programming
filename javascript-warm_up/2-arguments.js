69b064b1d4c3bec8a0a3ee90d74e34a9c69f5d34
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-06-14 13:40:46 +0200

    fix sembast linting errors

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (argv.slice(2).length == 0) {
+if (argv.slice(2).length === 0) {
   console.log('No argument');
-} else if (argv.slice(2).length == 1) {
+} else if (argv.slice(2).length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const argv = process.argv;
-const firstArg = argv[2];
 const intArg = parseInt(argv[2], 10);
 if (isNaN(intArg)) {
   console.log('Missing size');
