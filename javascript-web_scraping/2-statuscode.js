466429ff670eff26b8c66523bb695e90d6f551b5
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-06-28 17:51:55 +0200

    refactor status code file

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const { request } = require("node:http");
+const { request } = require('request');
 
 const url = process.argv[2];
-const resp = request(url, (error, response) => {
+request(url, (error, response) => {
   if (error) {
     console.log(error);
   }
