4ec1c41f67ba05d878e2959bb5a08fdd92a3b122
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-06-28 18:20:10 +0200

    add answer for completed tasks

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) console.log(error);
+  const resulr = {};
+  (JSON.parse(body),
+    filter((el) => el.completed).forEach((el) => {
+      if (result[el.userId] === undefined) {
+        result[el.userId] = 1;
+      } else {
+        result[el.userId] += 1;
+      }
+    }));
+  console.log(result);
+});
